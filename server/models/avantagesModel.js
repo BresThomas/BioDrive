6195303f4e81629ac5094a38c96300214939b4dd
bitoun nathan <nathan.bitoun@etu.univ-cotedazur.fr>
2024-03-13 14:09:11 +0100

    ajout Models restant

---
+++ server/models/avantagesModel.js
@@ -0,0 +1,10 @@
+class Avantage {
+    constructor(id_avantage, montant_bonus, tranches_bonus, montant_minimum) {
+        this.id_avantage = id_avantage;
+        this.montant_bonus = montant_bonus;
+        this.tranches_bonus = tranches_bonus;
+        this.montant_minimum = montant_minimum;
+    }
+}
+
+export default Avantage;

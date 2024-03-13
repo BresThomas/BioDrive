7bf3ebb0ac46a20c0c9040413bdef71fc0bf9b7b
Thomas Bres <thomas.bres@etu.unice.fr>
2024-03-13 14:30:47 +0100

    correction nom fichier avantageModel.js

--- server/index.js
+++ server/index.js
@@ -3,14 +3,33 @@ import cors from 'cors';
 
 import config from './Config.js';
 import productRoute from './routes/productRoute.js';
+import transactionRoute from './routes/transactionRoute.js';
+import carburantRoute from './routes/carburantRoute.js';
+import tacheRoute from './routes/tacheRoute.js';
+import pompeRoute from './routes/pompeRoute.js';
+import paiementRoute from './routes/paiementRoute.js';
+import employeRoute from './routes/employeRoute.js';
+import avantageRoute from './routes/avantageRoute.js';
+import clientRoute from './routes/clientRoute.js';
+import compteEnergieRoute from './routes/compteEnergieRoute.js';
+import incidentRoute from './routes/incidentRoute.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//routes
 app.use('/api', productRoute);
+app.use('/api', transactionRoute);
+app.use('/api', carburantRoute);
+app.use('/api', tacheRoute);
+app.use('/api', pompeRoute);
+app.use('/api', paiementRoute);
+app.use('/api', employeRoute);
+app.use('/api', avantageRoute);
+app.use('/api', clientRoute);
+app.use('/api', compteEnergieRoute);
+app.use('/api', incidentRoute);
 
 app.get("/hello", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -19,5 +38,3 @@ app.get("/hello", (req, res) => {
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl}`),
 );
-
-

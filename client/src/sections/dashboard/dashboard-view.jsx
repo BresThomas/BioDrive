c234d680fc6395a8fb3b32184c6d300f13b7fc98
Sébastien Lallement <sebastien.lallement@etu.unice.fr>
2024-03-27 11:05:24 +0100

    implementation ajout client fini

--- client/src/sections/dashboard/dashboard-view.jsx
+++ client/src/sections/dashboard/dashboard-view.jsx
@@ -17,18 +17,11 @@ import InputAdornment from '@mui/material/InputAdornment';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 
-import { useNavigate } from 'react-router-dom';
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from '../../Firebase';
-
 import PostSearch from '../blog/post-search';
 import Iconify from '../../components/iconify';
 import { RouterLink } from '../../routes/components';
 import { usePathname, useRouter } from '../../routes/hooks';
 import Popup from '../../components/popup/popup';
-import AccountPopover from '../../layouts/dashboard/common/account-popover';
-import NotificationsPopover from '../../layouts/dashboard/common/notifications-popover';
-
 
 
 import AppTasks from '../overview/app-tasks';
@@ -46,40 +39,39 @@ import { NAV } from '../../layouts/dashboard/config-layout';
 import navConfig from '../../layouts/dashboard/config-navigation';
 import { posts } from '../../_mock/blog';
 
-
 // ----------------------------------------------------------------------
 
 
 const paymentModes = ['Cartes bancaire', 'Liquide'];
 export default function DashboardView() {
-  
+
   const [enteredValue, setEnteredValue] = useState('');
-  
+
   const handleValueChange = (newValue) => {
     console.log("New value:", newValue);
     setEnteredValue(newValue);
   };
-  
-  const theme = useTheme();
-  const router = useRouter();
-  
-  const [products, setProducts] = useState([]);
 
-  const navigate = useNavigate();
-    useEffect(()=>{
-      onAuthStateChanged(auth, (user) => {
-          if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            const uid = user.uid;
-            navigate('/dashboard'); 
+    const theme = useTheme();
+    const router = useRouter();
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      fetch('http://localhost:3001/api')
+        .then((response) => response.json())
+        .then((data) => {
+          // Assurez-vous que les données sont un tableau
+          if (Array.isArray(data)) {
+            setProducts(data);
           } else {
-            // User is signed out
-            navigate('/login'); 
+            console.error("Les données reçues ne sont pas un tableau.");
           }
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la récupération des données:", error);
         });
-       
-  }, [navigate])
+    }, []);
 
     const handleClick = () => {
       router.push('/dashboard');
@@ -110,78 +102,6 @@ export default function DashboardView() {
       });
     };
 
-    const [incidents, setIncident] = useState([]);
-
-    useEffect(() => {
-      fetch('http://localhost:3001/api/incidents')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Erreur lors de la récupération des données');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setIncident(data);
-        })
-        .catch(error => {
-          console.error("Erreur lors de la récupération des données:", error);
-        });
-    }, []);
-
-    const [stocks, setStock] = useState([]);
-
-    useEffect(() => {
-      fetch('http://localhost:3001/api/stocks')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Erreur lors de la récupération des données');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setStock(data);
-        })
-        .catch(error => {
-          console.error("Erreur lors de la récupération des données:", error);
-        });
-    }, []);
-
-    const [pompes, setPompe] = useState([]);
-
-    useEffect(() => {
-      fetch('http://localhost:3001/api/pompes')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Erreur lors de la récupération des données');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setPompe(data);
-        })
-        .catch(error => {
-          console.error("Erreur lors de la récupération des données:", error);
-        });
-    }, []);
-
-    const [clients, setClient] = useState([]);
-
-    useEffect(() => {
-      fetch('http://localhost:3001/api/clients')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Erreur lors de la récupération des données');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setClient(data);
-        })
-        .catch(error => {
-          console.error("Erreur lors de la récupération des données:", error);
-        });
-    }, []);
-
     const [paymentMode, setPaymentMode] = useState('');
 
     // =============================CLIENT====================================== //
@@ -199,8 +119,8 @@ export default function DashboardView() {
   
     const handleChangeClient = (event) => {
       const { name, value } = event.target;
-      setFormDataClient(prevFormData => ({
-        ...prevFormData,
+      setFormDataClient(prevFormDataClient => ({
+        ...prevFormDataClient,
         [name]: value
       }));
     };
@@ -222,14 +142,12 @@ export default function DashboardView() {
           adresse: formDataClient.adresse_post,
         })
       });
-  
-      console.log(response);
-      console.log(response.ok);
 
       if (response.ok) {
         // Réinitialiser les champs du formulaire à leur valeur initiale vide
         setFormDataClient(initialFormDataClient);
         console.log("Formulaire soumis avec succès!");
+        window.location.reload(false);
       } else {
         console.error("Erreur lors de la soumission du formulaire");
       }
@@ -240,12 +158,12 @@ export default function DashboardView() {
       <Stack spacing={3} direction="row" alignItems="center">
         <Typography variant="h6" sx={{ width: '25%' }}>{title}</Typography>
         <Stack spacing={3} direction="row" alignItems="center">
-          <TextField name="email" label="Email" sx={{ width: '40%' }} onChange={handleChangeClient}/>
-          <TextField name="nom" label="Nom" sx={{ width: '40%' }} onChange={handleChangeClient}/>
-          <TextField name="prenom" label="Prénom" sx={{ width: '40%' }} onChange={handleChangeClient}/>
-          <TextField name="tel" label="Tel." sx={{ width: '40%' }} onChange={handleChangeClient}/>
-          <TextField name="adresse_post" label="Adresse Post." sx={{ width: '40%' }} onChange={handleChangeClient}/>
-          <TextField name="date_naissance" label="Date de Naissance" sx={{ width: '40%' }} onChange={handleChangeClient}/>
+          <TextField name="email" value={formDataClient.email} label="Email" sx={{ width: '40%' }} onChange={handleChangeClient}/>
+          <TextField name="nom" value={formDataClient.nom} label="Nom" sx={{ width: '40%' }} onChange={handleChangeClient}/>
+          <TextField name="prenom" value={formDataClient.prenom} label="Prénom" sx={{ width: '40%' }} onChange={handleChangeClient}/>
+          <TextField name="tel" value={formDataClient.tel} label="Tel." sx={{ width: '40%' }} onChange={handleChangeClient}/>
+          <TextField name="adresse_post" value={formDataClient.adresse_post} label="Adresse Post." sx={{ width: '40%' }} onChange={handleChangeClient}/>
+          <TextField name="date_naissance" value={formDataClient.date_naissance} label="Date de Naissance" sx={{ width: '40%' }} onChange={handleChangeClient}/>
         </Stack>
         <LoadingButton
           sx={{ width: '22.5%' }}
@@ -272,8 +190,8 @@ export default function DashboardView() {
   
     const handleChangeIncident = (event) => {
       const { name, value } = event.target;
-      setFormDataIncident(prevFormData => ({
-        ...prevFormData,
+      setFormDataIncident(prevFormDataIncident => ({
+        ...prevFormDataIncident,
         [name]: value
       }));
     };
@@ -307,6 +225,7 @@ export default function DashboardView() {
         // Réinitialiser les champs du formulaire à leur valeur initiale vide
         setFormDataIncident(initialFormDataIncident);
         console.log("Formulaire soumis avec succès!");
+        window.location.reload(false);
       } else {
         console.error("Erreur lors de la soumission du formulaire");
       }
@@ -432,11 +351,9 @@ export default function DashboardView() {
 
       <Grid container spacing={3}>
           <Grid item xs={12} sm={12} md={5} xl={5}>
-            <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2} sx={{ p: 2 }}>
                 <Typography variant="h4" sx={{ mb: 2, mt: 5 }}>
                 Caisse 💶
                 </Typography>
-            </Stack>
             <Grid container spacing={3}>
               <Grid item >
                 <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
@@ -468,7 +385,6 @@ export default function DashboardView() {
                       <AppNewsUpdate
                       sx={{height: 400, overflowY: 'auto' }}
                         title="Panier du client 🛒"
-                        path="/user"
                         list={[...Array(5)].map((_, index) => ({
                           id: faker.string.uuid(),
                           title: faker.person.jobTitle(),
@@ -489,21 +405,15 @@ export default function DashboardView() {
               </Grid>
             </Grid>
         <Grid item xs={36} sm={12} md={7} xl={7}>
-          <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2} sx={{ p: 2 }}>
-            <Typography variant="h4" sx={{ mb: 2, mt: 5 }}>
-              ERP 👋
-            </Typography>
-            <Stack direction="row" alignItems="center" spacing={1}>
-              <NotificationsPopover />
-              <AccountPopover />
-            </Stack>
-          </Stack>
+          <Typography variant="h4" sx={{ mb: 2, mt: 5 }}>
+            ERP 👋
+          </Typography>
           <Stack direction="row" spacing={2} sx={{ p: 2 }}>
             {navConfig.map((item) => (
               <NavItem key={item.title} item={item} />
             ))}
           </Stack>
-          
+
         <Grid container spacing={3}>
 
           <Grid xs={12} sm={6} md={3}>
@@ -518,65 +428,56 @@ export default function DashboardView() {
               </Card>
             </Stack>
           </Grid>
-
-          <Grid container spacing={3}>
           <Grid container spacing={3}> 
           <Grid xs={12} md={6} lg={4}>
             <AppNewsUpdate sx={{ width: 520, height: 200, overflowY: 'auto' }}
               title="Derniers incidents ⚠️"
-              path="/incidents"
-              list={incidents.slice(0,5).map(incident => ({
-                id: incident.id_incident,
-                title: `Gravité : ${incident.gravite}`,
-                description: incident.intitule, // Utilisez une description appropriée si disponible
-                image: `https://www.maison-kayser.com/1950-large_default/coca-cola-50-cl.jpg`, // Utilisez une logique appropriée pour l'image
-                postedAt: "03/20/2024", // Utilisez une date appropriée si disponible
+              list={[...Array(5)].map((_, index) => ({
+                id: faker.string.uuid(),
+                title: faker.person.jobTitle(),
+                description: faker.commerce.productDescription(),
+                image: `/assets/images/covers/cover_${index + 1}.jpg`,
+                postedAt: faker.date.recent(),
               }))}
-
             />
           </Grid>
-          
             <Grid xs={12} md={6} lg={4}>
-            <AppNewsUpdate
-              sx={{ width: 520, height: 200, overflowY: 'auto' }}
-              title="Consulter les stocks 📦"
-              path="/stocks"
-              list={stocks.slice(0,5).map(stock => ({
-                id: stock.id_stock,
-                title: stock.stock_details,
-                description: ` : ${stock.details ? stock.details.join(", ") : ''}`, // Vérifiez si stock.details est défini avant d'utiliser join
-                image: `https://www.maison-kayser.com/1950-large_default/coca-cola-50-cl.jpg`, // Utilisez une logique appropriée pour l'image
-                postedAt: "02/03/2020", // Utilisez une date appropriée si disponible
-              }))}
-            />
+              <AppNewsUpdate
+                sx={{ width: 520, height: 200, overflowY: 'auto' }}
+                title="Consulter les stocks 📦"
+                list={[...Array(5)].map((_, index) => ({
+                  id: faker.string.uuid(),
+                  title: faker.person.jobTitle(),
+                  description: faker.commerce.productDescription(),
+                  image: `/assets/images/covers/cover_${index + 1}.jpg`,
+                  postedAt: faker.date.recent(),
+                }))}
+              />
             </Grid>
           </Grid>
         <Grid container spacing={3}> 
           <Grid xs={12} md={6} lg={4}>
             <AppNewsUpdate sx={{ width: 520, height: 200, overflowY: 'auto' }}
               title="Pompes ⛽"
-              path="/pompes"
-              list={pompes.slice(0,5).map(pompe => ({
-                id: pompe.id_pompe,
-                title: pompe.id_pompe,
-                description: `Carburants : ${pompe.carburants.join(", ")}`, // Utilisez une description appropriée si disponible
-                image: `https://cdn-icons-png.flaticon.com/512/115/115101.png`, // Utilisez une logique appropriée pour l'image
-                postedAt: "02/03/2020", // Utilisez une date appropriée si disponible
+              list={[...Array(5)].map((_, index) => ({
+                id: faker.string.uuid(),
+                title: faker.person.jobTitle(),
+                description: faker.commerce.productDescription(),
+                image: `/assets/images/covers/cover_${index + 1}.jpg`,
+                postedAt: faker.date.recent(),
               }))}
-
             />
           </Grid>
             <Grid xs={12} md={6} lg={4}>
               <AppNewsUpdate
                 sx={{ width: 520, height: 200, overflowY: 'auto' }}
                 title="Rechercher client 👤"
-                path="/clients"
-                list={clients.slice(0,5).map(client => ({
-                  id: client.id_client,
-                  title: client.nom + client.prenom,
-                  description: `Adresse : ${client.adresse} Num : ${client.numero_portable} Date de naissance : ${client.date_naissance}`, // Utilisez une description appropriée si disponible
-                  image: `https://www.maison-kayser.com/1950-large_default/coca-cola-50-cl.jpg`, // Utilisez une logique appropriée pour l'image
-                  postedAt: "02/03/2020", // Utilisez une date appropriée si disponible
+                list={[...Array(5)].map((_, index) => ({
+                  id: faker.string.uuid(),
+                  title: faker.person.jobTitle(),
+                  description: faker.commerce.productDescription(),
+                  image: `/assets/images/covers/cover_${index + 1}.jpg`,
+                  postedAt: faker.date.recent(),
                 }))}
               />
             </Grid>
@@ -596,7 +497,6 @@ export default function DashboardView() {
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
   </Container>
 
   );

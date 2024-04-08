3ef5a81f2569b324611fc4e5d4b162a42f38a668
Thomas Bres <thomas.bres@etu.unice.fr>
2024-04-08 15:51:13 +0200

    update employé

--- client/src/_mock/form/AjouterClient.jsx
+++ client/src/_mock/form/AjouterClient.jsx
@@ -122,7 +122,6 @@ const AjouterClient = () => {
         <Card
           sx={{
             position: 'absolute',
-            width: 200,
             p: 4,
             top: '50%',
             left: '50%',
@@ -130,7 +129,7 @@ const AjouterClient = () => {
           }}
         >
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Horaires mis à jour
+            Client ajouté
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Le client a été ajouté avec succès.
